19699b32a53875bbb565d8e10037dc53c15d4c09
wanderer <wanderer00a1@users.noreply.github.com>
2025-06-14 16:41:57 +0530

    Format

--- Timer/src/App.tsx
+++ Timer/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Calculator from "./calculator";
+import Calculator from "./Calculator";
 import ToggleSounds from "./ToogleSounds";
 
 function App() {
